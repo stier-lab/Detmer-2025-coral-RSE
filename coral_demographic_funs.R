55d19d7b5c7f8af7966ec300345dfa0413b754c3
raine-detmer <adetmer@ucsb.edu>
2025-06-11 12:06:23 -0700

    code for demographic functions

--- coral_demographic_funs.R
+++ coral_demographic_funs.R
@@ -1,2 +1,7 @@
 #' README: demographic functions for creating datasets with coral growth, survival, and
 #' reproduction parameters for each time step in the model simulation
+
+# growth
+
+
+# survival
